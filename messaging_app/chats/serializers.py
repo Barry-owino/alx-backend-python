5c606a40e10eb948c6c00715f641ef2876df820a
Barry-owino <barryowino5@gmail.com>
2025-07-18 16:45:58 +0300

    created serializers for many_to_many

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import CustomUser, Conversation, Message
+
+# 1. User Serializer
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'profile_photo']
+
+# 2. Message Serializer
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = ['message_id', 'sender', 'message_body', 'sent_at', 'is_read']
+
+# 3. Conversation Serializer
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = ['conversation_id', 'participants', 'created_at', 'messages']
+
