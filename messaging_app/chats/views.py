bc4856c509ff7ae07e08dfa9650fbfefd12dbbb3
Barry-owino <barryowino5@gmail.com>
2025-07-18 17:14:29 +0300

    created views.py for the chats

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Only show conversations that include the current user
+        return self.queryset.filter(participants=self.request.user)
+
+    def perform_create(self, serializer):
+        # Add the logged-in user as a participant in the new conversation
+        conversation = serializer.save()
+        conversation.participants.add(self.request.user)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Only return messages from conversations the user is part of
+        return self.queryset.filter(conversation__participants=self.request.user)
+
+    def perform_create(self, serializer):
+        # Automatically set the sender to the logged-in user
+        serializer.save(sender=self.request.user)
+
