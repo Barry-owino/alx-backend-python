c754dbae697886473bebd113bcba270bada217cf
Barry-owino <barryowino5@gmail.com>
2025-09-10 21:43:37 +0300

    register models in the Djnago admin

--- Django-signals_orm-0x04/messaging/admin.py
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Message, Notification
 
-# Register your models here.
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sender', 'receiver', 'content', 'timestamp')
+    list_filter = ('sender', 'receiver', 'timestamp')
+    search_fields = ('content',)
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'message', 'timestamp', 'read')
+    list_filter = ('user', 'read', 'timestamp')
+
