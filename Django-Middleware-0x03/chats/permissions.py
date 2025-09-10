7c9a371f897adddb94923a8b33a586e98b474749
Barry-owino <barryowino5@gmail.com>
2025-09-10 17:43:21 +0300

    duplicated and renamed the folder

---
+++ Django-Middleware-0x03/chats/permissions.py
@@ -0,0 +1,20 @@
+from rest_framework import permissions
+from .models import Conversation
+
+class IsParticipantOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow participants of a conversation
+    to view or interact with it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # For conversations
+        if hasattr(obj, 'participants'):
+            return request.user in obj.participants.all()
+
+        # For messages (check if user is part of the conversation)
+        if hasattr(obj, 'conversation'):
+            return request.user in obj.conversation.participants.all()
+
+        return False
+
