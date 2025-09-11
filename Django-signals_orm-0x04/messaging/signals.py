8ad42f7f45de7f5bff3304a76ed67dbce1a4f9ee
Barry-owino <barryowino5@gmail.com>
2025-09-11 04:25:22 +0300

    Implemented post_delete signal

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import pre_save
 from django.db.models.signals import post_save
+from django.db.models.signal import post_delete
 from django.dispatch import receiver
 from .models import Message, Notification, MessageHistory
 
@@ -24,3 +25,19 @@ def log_message_edit(sender, instance, **kwargs):
                 old_content=old_message.content
             )
             instance.edited = True  # mark message as edited
+
+@receiver(post_delete, sender=User)
+def delete_user_related_data(sender, instance, **kwargs):
+    """
+    Deletes all messages, notifications, and message history associated with the deleted user.
+    """
+    # Delete messages sent or received
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+
+    # Delete notifications
+    Notification.objects.filter(user=instance).delete()
+
+    # Delete message histories for messages sent/received by the user
+    MessageHistory.objects.filter(message__sender=instance).delete()
+    MessageHistory.objects.filter(message__receiver=instance).delete()
